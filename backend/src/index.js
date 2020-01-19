df342a9b4927d53ee09a5a3bb90f2df0292daa9f
pdavinunes <p.davi.nunes@gmail.com>
2020-01-18 23:15:48 -0300

    Criando socket no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose')
 const cors = require('cors');
 const routes = require('./routes')
+const http = require('http')
+const {setupWebSocket} = require('./websocket') 
 
 const app = express();
+const server = http.Server(app)
+
+setupWebSocket(server)
 
 //mongodb+srv://davinunes:<password>@cluster0-keo1v.mongodb.net/test?retryWrites=true&w=majority
 mongoose.connect('mongodb://davinunes:admin@cluster0-shard-00-00-keo1v.mongodb.net:27017,cluster0-shard-00-01-keo1v.mongodb.net:27017,cluster0-shard-00-02-keo1v.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', 
@@ -26,4 +31,4 @@ app.use(routes)
 
 // MongoDB (Não-relacional)
 
-app.listen(3333);
+server.listen(3333);
